6c259b8eef04eb5bb4aae98a2ba8d0d55c7ff46c
camiloATL <catorres0610@gmail.com>
2023-05-01 16:11:42 -0500

    Test CSS

--- tec-sas-webpage/src/App.js
+++ tec-sas-webpage/src/App.js
@@ -10,9 +10,102 @@ function App() {
       </div>
       <div className="line"></div>
       <div className="contenedor">
-        <div className="titanlogo"></div>
-        <div className="stylelogin"></div>
-        <div className="codeblocklogo"></div>
+        <div className="stylelogin">
+          <form className="row g-3">
+            <div className="col-md-6">
+              <label htmlFor="inputEmail4" className="form-label">
+                Nombres
+              </label>
+              <input type="email" className="form-control" id="inputEmail4" />
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="inputPassword4" className="form-label">
+                Apellidos
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword4"
+              />
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="inputEmail4" className="form-label">
+                Email
+              </label>
+              <input type="email" className="form-control" id="inputEmail4" />
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="inputPassword4" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword4"
+              />
+            </div>
+            <div className="col-12">
+              <label htmlFor="inputAddress" className="form-label">
+                Address
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress"
+                placeholder="1234 Main St"
+              />
+            </div>
+            <div className="col-12">
+              <label htmlFor="inputAddress2" className="form-label">
+                Address 2
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress2"
+                placeholder="Apartment, studio, or floor"
+              />
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="inputCity" className="form-label">
+                City
+              </label>
+              <input type="text" className="form-control" id="inputCity" />
+            </div>
+            <div className="col-md-4">
+              <label htmlFor="inputState" className="form-label">
+                State
+              </label>
+              <select id="inputState" className="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="col-md-2">
+              <label htmlFor="inputZip" className="form-label">
+                Zip
+              </label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
+            <div className="col-12">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="gridCheck"
+                />
+                <label className="form-check-label" htmlFor="gridCheck">
+                  Check me out
+                </label>
+              </div>
+            </div>
+            <div className="col-12">
+              <button type="submit" className="btn btn-primary">
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
       <div className="ancho-arriba"></div>
     </div>
